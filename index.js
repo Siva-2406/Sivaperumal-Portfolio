bec15b092e6828f1b2e0fa05f4620f072b8c6de9
Sivaperumal Balasubramaniyam <sbalasubramaniyam@freshworks.com>
2024-09-02 17:09:13 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let k=10;
+let g=-10;
+if(k>0){
+    console.log("The number is positive");
+}
+else{
+    console.log("The number is Negative")
+}
+
+let a="a";
+let b="e";
+let c="i";
+let d="o";
+let e="u";
+if(a&&e){
+    console.log("The character is a vowel");
+}else{
+    console.log("The character is not a vowel")
+}
