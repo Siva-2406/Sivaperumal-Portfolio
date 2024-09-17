1c80bd85ed2c8ce665b636e157516e32c8f3473c
Siva-2406 <bsivaperumal2006@gmail.com>
2024-09-17 16:03:35 +0530

    Sivaperumal portfolio

--- index.js
+++ index.js
@@ -1,19 +1,286 @@
-let k=10;
-let g=-10;
-if(k>0){
-    console.log("The number is positive");
-}
-else{
-    console.log("The number is Negative")
-}
+// // // // // let k=10;
+// // // // // let g=-10;
+// // // // // if(k>0){
+// // // // //     console.log("The number is positive");
+// // // // // }
+// // // // // else{
+// // // // //     console.log("The number is Negative")
+// // // // // }
 
-let a="a";
-let b="e";
-let c="i";
-let d="o";
-let e="u";
-if(a&&e){
-    console.log("The character is a vowel");
-}else{
-    console.log("The character is not a vowel")
+// // // // // let a="a";
+// // // // // let b="e";
+// // // // // let c="i";
+// // // // // let d="o";
+// // // // // let e="u";
+// // // // // if(a&&e){
+// // // // //     console.log("The character is a vowel");
+// // // // // }else{
+// // // // //     console.log("The character is not a vowel")
+// // // // // }
+
+
+// // // // let jerseyNo=100;
+// // // // if(jerseyNo==7){
+// // // //     console.log("Dhoni");
+// // // // }else if(jerseyNo==10){
+// // // //     console.log("Sachin tendulkar")
+// // // // }else if(jerseyNo==100){
+// // // // console.log("Prithvi Shaw")
+// // // // }else{
+// // // //     console.log("Inavalid response")
+// // // // }
+
+
+
+
+
+// // // let p=40000;
+// // // let n=7;
+// // // let rateOfInterest=40% 
+// // // let simpleInterest=(pnrateOfInterest/1000);
+// // // if(simpleInterest>50){
+// // //     console.log("greater than 50");
+// // // }else if(simpleInterest<50){
+// // //     console.log("lesser than 50")
+// // // }else{
+// // //     console.log("invalid")
+// // // }
+
+
+// // // let tamil=71;
+// // // let english=82;
+// // // let maths=50;
+// // // let science=67;
+// // // let socialScience=69;
+// // // let average=(tamil+english+maths+science+socialScience)/5
+// // // if(average>50%){
+// // //     console.log("pass")
+// // // }else {
+//  //// con}
+
+
+// // // 
+
+
+
+// // let rno=1001;
+// // while(rno<1028){
+// //     console.log("Rno:", rno);
+// //     rno++
+// // }
+
+
+
+
+// // }
+
+
+// // 
+
+
+// printTable(2, 10);
+// printTable(3, 100);
+// printTable(4, 1000);
+
+// function printTable(tableValue, mValue){
+//     let count =1;
+//     while(count<=mValue){
+//         console.log(tableValue, "*", count, "=", tableValue*count);
+//         count++
+//     }
+// }
+
+// 
+
+
+
+
+
+// let tables=1;
+// while(tables<=10){
+//   console.log("3*", tables, "=",3*tables);
+//   tables++
+// }
+
+// 
+
+
+// 
+
+
+// userPin= 1233;
+// do {
+//    userPin = ("Enter your 4-digit PIN:");
+
+//    if (userPin !== STORED_PIN) {
+//        attempts++;
+//        alert(`Incorrect PIN. Attempts remaining: ${MAX_ATTEMPTS - attempts}`);
+//    }
+// } while (userPin !== STORED_PIN && attempts < MAX_ATTEMPTS);
+
+// if (userPin === STORED_PIN) {
+//    alert("PIN correct. Welcome!");
+// } else {
+//    alert("Account locked. Maximum attempts reached.");
+// }
+
+
+
+
+
+// let number=11;
+// while(number>0){ 
+//      number--
+//     console.log(number , number);
+// }
+// let firstTwoNumbers1 = addTwoNumbers(30,40);
+// console.log(firstTwoNumbers);  
+
+// function addTwoNumbers(a,b) {
+//    return a+b;
+//  }
+
+//  let firstTwoNumbers2 = addTwoNumbers(30,40);
+// console.log(firstTwoNumbers);  
+
+// function addTwoNumbers(a,b) {
+//    return a-b;
+//  }
+
+//  let firstTwoNumbers3 = addTwoNumbers(30,40);
+// console.log(firstTwoNumbers);  
+
+// function addTwoNumbers(a,b) {
+//    return a+b;
+//  }
+
+//  let firstTwoNumbers4 = addTwoNumbers(30,40);
+// console.log(firstTwoNumbers);  
+
+// function addTwoNumbers(a,b) {
+//    return a/b;
+//  }
+
+// console.log("Sivaperumal B"funName);
+// console.log(2406 + funname);
+// console.log("freshworks STS Software academy"funName)
+// function  funName(value){
+//    return value
+// }
+
+// 
+
+// let temperature = 40;
+// if (temperature <= 20) {
+//  console.log("It's cold outside.");
+// } else {
+//  console.log("It's warm outside.");
+// }
+
+
+// let n=8;
+//     let sum = 0;
+//     let i = 1;
+//     while (i <= n) {
+//         sum += i;
+//         i++;
+//     }
+//     function sumNumbers(n)
+//      return sumNumers;
+// Factorial(3);
+// function Factorial(n) {
+//     let fact= n;
+//     let i = 1;
+//     while (i <= n) {
+//         fact=fact*i;
+//         console.log(fact);
+//         i++;
+//     }
+// }
+
+// fact(3);
+// function fuction(n){
+   
+//     while()
+// }
+
+// for loop syntax
+// for(x=1;x<=5;x++){
+//     console.log(x);
+// }
+// for(x=5;x>=0;x--){
+//     console.log(x);
+// }
+// let sum=0
+// for(x=1;x<=5;x++){
+//     sum=sum+1;
+//     console.log(x);
+// }
+
+ 
+
+
+// let greeting = 'Hello, World!';
+// let name = 'John Doe';
+// let message = `Welcome to the JavaScript world, ${name}!`;
+
+// console.log(greeting); // Outputs: Hello, World!
+// console.log(name); // Outputs: John Doe
+// console.log(message);
+
+
+
+
+
+// value(1);
+// function value(n){
+// while(n<=100){
+//     if(n/3==0){
+//         console.log(n);
+//     }else{
+//         console.log("Invalid")
+//     }
+// }
+//     console.log(n);
+//     n++
+
+// }
+
+// value(1,100);
+// function value(initial,final){
+//     while(initial<=final){
+//         if(initial%3==0){
+//             console.log(initial);
+//         }
+//         // console.log(initial);
+//         initial++;
+
+//     } 
+// }
+
+
+// let value="*";
+// let number=1
+// while(1<=5){
+//     value=value+number+"";
+//     console.log(value);
+//     number++
+// }
+
+
+
+// ui(5);
+// function ui(y){
+//     y=y+2;
+//     console.log(y)
+value(1);
+function value(n){
+    if(n%2==0){
+        print("Even number");
+    }else{
+        print("Odd number");
+    }
 }
+function print(n){
+    console.log(n)
+    }
